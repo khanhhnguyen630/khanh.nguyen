5860f8f3992d4815d3635886d71109c1e691f6dc
khanhhnguyen630 <148497900+khanhhnguyen630@users.noreply.github.com>
2023-10-30 23:46:08 -0700

    New port

--- portfolio3-1/script.js
+++ portfolio3-1/script.js
@@ -1,38 +1,39 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   let typed = new Typed(".auto-typed"), {
-//     strings: ["Student", "educator"]
-//     typeSpeed: 150,
-//     backSpeed: 150,
-//     loop: true,
-//   });
-// });
-
-document.adddEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
+  // Get references to the menu icon and the navigation menu
   const menuIcon = document.getElementById('menu-icon');
   const navMenu = document.getElementById('nav-menu');
 
+  // Function to show/hide the navigation menu
   function toggleNavMenu() {
-    if (window .innerWidth < 789) {
+    if (window.innerWidth < 789) {
+      // On small screens, toggle the menu's display property
       if (navMenu.style.display === 'block' || navMenu.style.display === '') {
+        navMenu.style.display = 'none';
       } else {
-        navMenu.style.display = 'block'
+        navMenu.style.display = 'block';
       }
     } else {
+      // On larger screens (via media queries), always display the menu
       navMenu.style.display = 'block';
     }
   }
 
-  menuIcon.adddEventListener('click', function() {
+  // Add a click event listener to the menu icon
+  menuIcon.addEventListener('click', function () {
     toggleNavMenu();
-  })
-  
-  toggleNavMenu();
-  window.addEventListener('resize', toggleNavMenu);
-  
+  });
+
+  // Call the toggleNavMenu function initially and whenever the window is resized
+  toggleNavMenu(); // Call initially
+  window.addEventListener('resize', toggleNavMenu); // Call on resize
+
+  // Initialize the Typed.js library
   let typed = new Typed(".auto-type", {
-    strings: ["Student", "Educator"],
+    strings: ["Student", "Future Developer", "Artist"],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
-});
+  });
+
+  // You can add any other JavaScript code you need here.
 });
